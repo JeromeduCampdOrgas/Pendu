fb929710875079821fea75e22141fb1100e968cd
Jerome <ducampdorgasjerome@gmail.com>
2022-03-08 17:35:36 +0100

    affichage du mot à trouver si perdu

--- src/word-to-find.js
+++ src/word-to-find.js
@@ -1,63 +1,69 @@
 export function wordToFindIinit(word) {
-    let count = 0,
-        found =0;
-    // affiche les emplacements du mot
-    const wordLocation = document.getElementById("word-location");
-    word = word.toLowerCase().split("");
-    let i=0;
-    for (const char of word) {
-        const letter = document.createElement("span");
-        letter.classList.add("letter");
-        letter.setAttribute("data-id",i);
-        // letter.innerHTML = char;
-        wordLocation.append(letter);
-        i++;
-    }
-    // écoute sur les touches pour vérifier la concordance lettre / mot
-    const keyboard = document.querySelectorAll("#keyboard .letter");
-    for (const key of keyboard) {
-        key.addEventListener("click", function verifLetterInWord(e) {
-        const keyPushed = e.target;
-        const letterKeyPushed = keyPushed.innerText.toLowerCase();
-        if (word.includes(letterKeyPushed)) {
-            // lettre trouvée => affiche les lettres + modif touche
-            key.classList.add("find");
-            // word.forEach((letterWord,index) => (letterKeyPushed === letterWord) ? verifLetterInWord(letterWord,index) : ''); 
-            word.forEach( (letterWord,index) => {
-                if (letterKeyPushed === letterWord) {
-                    letterInSpan(letterKeyPushed, index);
-                    found++;
-                }
-            } );
-            if (found === word.length) {
-                displayResult("vous avez gagné !","win");
-            }
-        } else {
-            // lettre non trouvée => modif touche + compteur + pendu 
-            key.classList.add("notfind");
-            count++;
-            if (count === 6) {
-                displayResult("vous avez perdu !","loose");
-            }
+  let count = 0,
+    found = 0;
+  // affiche les emplacements du mot
+  const wordLocation = document.getElementById("word-location");
+  word = word.toLowerCase().split("");
+  let i = 0;
+  for (const char of word) {
+    const letter = document.createElement("span");
+    letter.classList.add("letter");
+    letter.setAttribute("data-id", i);
+    // letter.innerHTML = char;
+    wordLocation.append(letter);
+    i++;
+  }
+  // écoute sur les touches pour vérifier la concordance lettre / mot
+  const keyboard = document.querySelectorAll("#keyboard .letter");
+  for (const key of keyboard) {
+    key.addEventListener("click", function verifLetterInWord(e) {
+      const keyPushed = e.target;
+      const letterKeyPushed = keyPushed.innerText.toLowerCase();
+      if (word.includes(letterKeyPushed)) {
+        // lettre trouvée => affiche les lettres + modif touche
+        key.classList.add("find");
+        // word.forEach((letterWord,index) => (letterKeyPushed === letterWord) ? verifLetterInWord(letterWord,index) : '');
+        word.forEach((letterWord, index) => {
+          if (letterKeyPushed === letterWord) {
+            letterInSpan(letterWord, index);
+            found++;
+          }
+        });
+        if (found === word.length) {
+          displayResult("vous avez gagné !", "win");
         }
-        keyPushed.removeEventListener("click", verifLetterInWord);
-            key.classList.remove("active");
-      });
-}
+      } else {
+        // lettre non trouvée => modif touche + compteur + pendu
+        key.classList.add("notfind");
+        count++;
+        if (count === 6) {
+          word.forEach((letterWord, index) => {
+            letterInSpan(letterWord, index);
+            found++;
+          });
+          displayResult("vous avez perdu !", "loose");
+        }
+      }
+      keyPushed.removeEventListener("click", verifLetterInWord);
+      key.classList.remove("active");
+    });
+  }
 }
 
 export function letterInSpan(letter, index) {
-    // récupère le span correspondant à l'index pour afficher la lettre
-const spanLetter = document.querySelector('#word-location [data-id="'+index+'"]');
-spanLetter.innerHTML = letter.toUpperCase();
+  // récupère le span correspondant à l'index pour afficher la lettre
+  const spanLetter = document.querySelector(
+    '#word-location [data-id="' + index + '"]'
+  );
+  spanLetter.innerHTML = letter.toUpperCase();
 }
 
-function displayResult(result,classe) {
-    const insertResult = document.getElementById("result");
-    const insertResultH2 = document.createElement("h2");
-    insertResultH2.innerHTML = result.toUpperCase();
-    insertResultH2.classList.add(classe);
-    insertResult.append(insertResultH2);
+function displayResult(result, classe) {
+  const insertResult = document.getElementById("result");
+  const insertResultH2 = document.createElement("h2");
+  insertResultH2.innerHTML = result.toUpperCase();
+  insertResultH2.classList.add(classe);
+  insertResult.append(insertResultH2);
 }
 
 // export function verifLetterInWord(key,word) {
